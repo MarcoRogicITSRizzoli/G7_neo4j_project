77ba1d872f288801c77351ec8f8acfd46dfdd131
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-07-11 11:24:45 +0200

    connessione

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from neo4j import GraphDatabase
 
 URI = "neo4j+s://812006b6.databases.neo4j.io:7687"
-AUTH = ("michelpotsios50@gmail.com", "75iv38TCxoBIwqr32hDK99NVuDEmUDRcRxM87Hs4yZI")
+AUTH = ("neo4j", "75iv38TCxoBIwqr32hDK99NVuDEmUDRcRxM87Hs4yZI")
 
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
