b448f1d9e32d3e50e85ec446ab7e1deee43d12f2
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-07-11 10:28:50 +0200

    update connection database

--- database.py
+++ database.py
@@ -1,2 +1,7 @@
 from neo4j import GraphDatabase
 
+URI = "neo4j+s://b1bb3ceb.databases.neo4j.io"
+AUTH = ("", "<Password>")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
