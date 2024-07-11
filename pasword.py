081ddc2efb53294fc5d444582cf8f4a69567e464
Michele Potsios <michelpotsios50@gmail.com>
2024-07-11 11:16:47 +0200

    pasword

---
+++ pasword.py
@@ -0,0 +1,3 @@
+Python 3.12.4 (v3.12.4:8e8a4baf65, Jun  6 2024, 17:33:18) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+75iv38TCxoBIwqr32hDK99NVuDEmUDRcRxM87Hs4yZI
