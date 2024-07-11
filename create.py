0263dc324d0f49a54195ace8f6301a60032908f6
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-07-11 11:56:45 +0200

    created connect function

--- create.py
+++ create.py
@@ -1,7 +1,2 @@
 from database import *
 
-def create_nodes():
-    query = ("CREATE (p:Person {name: 'Giorgio Ciampi'})-[:Possiede]->(s:Sim {number: '+39333333333'})-[:Connesso]->(c:Telefono {id: '1', data: '2024-07-30})")
-    tx.run(query)
-    
-if __name__=="__main__":
--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from neo4j import GraphDatabase
 
-URI = "neo4j+s://812006b6.databases.neo4j.io:7687"
-AUTH = ("neo4j", "75iv38TCxoBIwqr32hDK99NVuDEmUDRcRxM87Hs4yZI")
+def connection():
+    URI = "neo4j+s://812006b6.databases.neo4j.io:7687"
+    AUTH = ("neo4j", "75iv38TCxoBIwqr32hDK99NVuDEmUDRcRxM87Hs4yZI")
 
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        driver.verify_connectivity()
