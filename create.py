5a449b657e3f542d69a391ada798d011479e6b0c
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-07-17 16:47:42 +0200

    update

--- create.py
+++ create.py
@@ -53,7 +53,7 @@ def create_connessa_a(sim_id, cella_id, datetime):
 def main():
     persona_id = create_persona("Giorgio", "Ciampi")
     sim_id = create_sim("123456789")
-    cella_id = create_cella("45.47709", "9.15385", "Zona Centrale")
+    cella_id = create_cella(45.47709, 9.15385, "Zona Centrale")
     create_posseduta_da(persona_id, sim_id)
     create_connessa_a(sim_id, cella_id, "2024-09-15T10:30:00")
     
@@ -67,9 +67,9 @@ def main():
     sim_id3 = create_sim('33333333')
     sim_id4 = create_sim('44444444')
     
-    cella_id1 = create_cella("41.890251", "12.492373","zona sud" )
-    cella_id2 = create_cella("45.464203", "9.189982", "zona est" )
-    cella_id3 = create_cella("40.851775", "14.268124","zona ovest" )
+    cella_id1 = create_cella(41.890251, 12.492373,"zona sud" )
+    cella_id2 = create_cella(45.464203, 9.189982, "zona est" )
+    cella_id3 = create_cella(40.851775, 14.268124,"zona ovest" )
     
     create_posseduta_da(persona_id1, sim_id1)
     create_posseduta_da(persona_id2, sim_id2)
