70594c58a60429db9ac656dbc929d36e34e7dca9
Marco Rogic <marco.rogic@itsrizzoli.it>
2024-07-17 17:37:05 +0200

    fix

--- richieste.py
+++ richieste.py
@@ -5,37 +5,40 @@ def find_cells_by_persona(data, ora, nome, cognome):
     with driver.session() as session:
         result = session.run(
             """
-            MATCH (p:Persona {nome: $nome, cognome: $cognome})-[:POSSIEDE]->(s:Sim)-[r:CONNESSO_A {data_ora: $data_ora}]->(c:Cella)
+            MATCH (p:Persona {nome: $nome, cognome: $cognome})-[:POSSIEDE]->(s:Sim)-[r:CONNESSO_A {date: $data, time: $ora}]->(c:Cella)
             RETURN c.id, c.latitudine, c.longitudine
             """,
             nome=nome, cognome=cognome, data_ora=f"{data}T{ora}"
         )
+        records = [record for record in result]
     driver.close()
-    return [record for record in result]
+    return records
 
 def find_personas_by_cell(data, ora, cella_id):
     driver = get_db()
     with driver.session() as session:
         result = session.run(
             """
-            MATCH (p:Persona)-[:POSSIEDE]->(s:Sim)-[r:CONNESSO_A {data_ora: $data_ora}]->(c:Cella {id: $cella_id})
+            MATCH (p:Persona)-[:POSSIEDE]->(s:Sim)-[r:CONNESSO_A {date: $data, time: $ora}]->(c:Cella {id: $cella_id})
             RETURN p.id, p.nome, p.cognome
             """,
             cella_id=cella_id, data_ora=f"{data}T{ora}"
         )
+        records = [record for record in result]
     driver.close()
-    return [record for record in result]
+    return records
 
 def find_personas_by_radius(data, ora, latitudine, longitudine, raggio):
     driver = get_db()
     with driver.session() as session:
         result = session.run(
             """
-            MATCH (p:Persona)-[:POSSIEDE]->(s:Sim)-[r:CONNESSO_A {data_ora: $data_ora}]->(c:Cella)
+            MATCH (p:Persona)-[:POSSIEDE]->(s:Sim)-[r:CONNESSO_A {date: $data, time: $ora}]->(c:Cella)
             WHERE point.distance(point({latitude: c.latitudine, longitude: c.longitudine}), point({latitude: $latitudine, longitude: $longitudine})) <= $raggio
             RETURN p.id, p.nome, p.cognome
             """,
             data_ora=f"{data}T{ora}", latitudine=latitudine, longitudine=longitudine, raggio=raggio
         )
+        records = [record for record in result]
     driver.close()
-    return [record for record in result]
+    return records
